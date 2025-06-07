99b5f269ad5a18a7c7b3a1f897f5f6aa71945323
piyyu <piyushkr446@gmail.com>
2025-06-07 10:47:53 +0530

    update

--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -1,14 +1,18 @@
-
-
-const NotesList = () => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-
+const NotesList = ({notes}) => {
   return (
-    <div>
-      
+    <div className="p-4 space-y-4">
+      {notes.length === 0 ? (
+        <div>No notes found</div>
+      ) : (
+        notes.map((note) => (
+          <div key={note._id} className="border p-4 rounded shadow">
+            <h2 className="font-bold text-xl">{note.title}</h2>
+            <p>{note.content}</p>
+          </div>
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default NotesList;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import {useNavigate} from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import NewNoteForm from '../components/NewNoteForm';
+import NotesList from '../components/NotesList';
 
 const Dashboard = () => {
+    const [notes, setNotes] = useState([]);
     const user = localStorage.getItem('user');
     const navigate = useNavigate();
 
@@ -14,13 +18,32 @@ const Dashboard = () => {
         navigate('/auth');
     }
 
+    const handleGetNotes = async () => {
+        try {
+        const response = await axios.get('http://localhost:3000/api/notes', {
+            headers: {
+            token: localStorage.getItem('token'),
+            },
+        });
+        setNotes(response.data.notes);
+        } catch (error) {
+        console.log("Error fetching notes:", error);
+        }
+    };
+
+    useEffect(() => {
+        handleGetNotes();
+    }, []);
+
   return (
     <div className="">
       <Header logout={Logout} />
       <div className="flex flex-col items-center justify-center min-h-screen text-center">
         <h1>Dashboard</h1>
+        <div>Hello, {user}</div>
+        <NewNoteForm onCreateNote={handleGetNotes} />
+        <NotesList notes={notes} />
       </div>
-      <NewNoteForm />
       <Footer />
     </div>
 )};
